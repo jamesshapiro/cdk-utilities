69329b76a6aadf47493e5bd5e6c6b363cf30991a
James Shapiro <jamesshapiro@gmail.com>
2022-04-05 02:25:30 -0400

    fix log parsing error

--- cloudfront-logs-example/functions/aws_analytics.py
+++ cloudfront-logs-example/functions/aws_analytics.py
@@ -15,10 +15,13 @@ def lambda_handler(event, context):
     print(f'{bucket}{key}')
     response = s3_client.get_object(Bucket=bucket, Key=key)
     content = response['Body'].read()
+    #with gzip.GzipFile(fileobj=io.BytesIO(content), mode='rb') as f:
     with gzip.GzipFile(fileobj=io.BytesIO(content), mode='rb') as f:
-        cloudfront_logs = json.load(f)
-    print(cloudfront_logs)
+        cloudfront_logs = f.read().splitlines()
+    cloudfront_logs = [line.decode("utf-8") for line in cloudfront_logs]
+    for log in cloudfront_logs:
+        print(log)
     return {
         'statusCode': 200,
-        'body': json.dumps(cloudfront_logs)
+        'body': cloudfront_logs
     }
